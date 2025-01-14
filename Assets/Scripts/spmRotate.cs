88153c9a13b347951ea956f5a660afac5c4bd8ba
MontTheMonkey <mont.lettered@gmail.com>
2025-01-14 17:00:42 -0500

    Tweaked rotate script so you can change the rotation speed (float variable)

--- Assets/Scripts/spmRotate.cs
+++ Assets/Scripts/spmRotate.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class spmRotate : MonoBehaviour
 {
+    public float hourHand = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,7 @@ public class spmRotate : MonoBehaviour
     void Update()
     {
         Vector3 zRotate = transform.eulerAngles;
-        zRotate.z += 0.5f;
+        zRotate.z += hourHand;
         transform.eulerAngles = zRotate;
     }
 }

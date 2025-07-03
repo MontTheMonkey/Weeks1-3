9b8148ebe681d9a2ff042d4300c89fd56bbc2663
Mont <mont.lettered@gmail.com>
2025-07-03 19:59:12 -0400

    Circle mover update
    Uses all GetKey methods now instead of only GetKeyDown

--- Assets/Scripts/circleMover.cs
+++ Assets/Scripts/circleMover.cs
@@ -15,18 +15,21 @@ public class circleMover : MonoBehaviour
     {
         Vector3 crlPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         //These three input methods check if an input is being pressed and returns a boolean [true or false]
-        bool rightKeyDown = Input.GetKeyDown(KeyCode.RightArrow);
+        //GetKey checks if the key is being pressed
+        //GetKeyDown checks once the key has been pressed down
+        //GetKeyUp checks once the key has been released
+        bool rightKey = Input.GetKey(KeyCode.RightArrow);
         bool upKeyDown = Input.GetKeyDown(KeyCode.UpArrow);
-        bool downKeyDown = Input.GetKeyDown(KeyCode.DownArrow);
-        if (rightKeyDown == true)
+        bool downKeyUp = Input.GetKeyUp(KeyCode.DownArrow);
+        if (rightKey == true)
         {
-            crlPos.x = crlPos.x + 0.5f;
+            crlPos.x = crlPos.x + 0.005f;
         }
         if (upKeyDown == true)
         {
             crlPos.y = crlPos.y + 0.1f;
         }
-        if (downKeyDown == true)
+        if (downKeyUp == true)
         {
             crlPos.y = crlPos.y - 0.1f;
         }
